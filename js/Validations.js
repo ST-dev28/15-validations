37cdc4769442045fbfb3a2ca27903e495a121e35
SandraT <sndrbltr@gmail.com>
2021-05-22 08:39:11 +0000

    Initial commit

---
+++ js/Validations.js
@@ -0,0 +1,16 @@
+/*
+VALIDACIJOS
+ar validus:
+- email
+- firstname
+- lastname
+- message
+- phone number
+- month name
+- weekday name
+- person ID (asmens kodas)  lyymmdd123x
+
+naudojame: OOP
+kiek imanoma optimizuoti - pasistengti nerasyti tu paciu validaciju 
+skirtingose vietose
+*/
