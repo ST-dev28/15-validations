42bc9dc9e17e1903b36c91cc92d3216748acba91
SandraT <sndrbltr@gmail.com>
2021-05-25 16:22:29 +0000

    Tests for average function

--- js/average/average.js
+++ js/average/average.js
@@ -1,11 +1,21 @@
-function average(list) {
-    
-    return (list.reduce(t + s, 0) / list.length);
-}
-    
+const average = list => {
+    if (list === undefined ||
+        !Array.isArray(list) ||
+        list.length === 0) {
+        return false;
+    }
 
-   
+    let totalSum = 0;
+    let validNumbersCount = 0;
 
+    for (const number of list) {
+        if (typeof number === 'number' && !isNaN(number)) {
+            totalSum += number;
+            validNumbersCount++;
+        }
+    }
 
+    return totalSum / validNumbersCount;
+};
 
 module.exports = average;
--- js/average/average.test.js
+++ js/average/average.test.js
@@ -1,22 +1,81 @@
 const average = require('./average');
 
-test('sum all and devide of quantity', () => {
-    expect(average(10, 20, 30)).toBe(20);
-});
+test('sarasas is vieno skaiciaus', () => {
+    expect(average([1])).toBe(1);
+})
 
-test('sum all and devide of quantity', () => {
-    expect(average(0, 1, 2, 3, 4, 5, 6)).toBe(3);
-});
+test('sarasas is triju skaiciu', () => {
+    expect(average([1, 2, 3])).toBe(2);
+})
 
-test('string is not allowed', () => {
-    expect(average('abc', 8, 10)).toBeFalsy();
-});
+test('sarasas is teigiamu ir neigiamu skaiciu', () => {
+    expect(average([-1, 0, 1])).toBe(0);
+})
 
-test('string and NaN are not allowed', () => {
-    expect(average(NaN, 'abc', 20)).toBeFalsy();
-});
+test('vidurkis, kai neduota jokia reiksme', () => {
+    expect(average()).toBe(false);
+})
 
-test('NaN is not allowed', () => {
-    expect(average(10, NaN, 8)).toBe(false);
-});
+test('vidurkis, kai duotas ne array: string', () => {
+    expect(average('adsf')).toBe(false);
+})
 
+test('vidurkis, kai duotas ne array: boolean', () => {
+    expect(average(true)).toBe(false);
+})
+
+test('vidurkis, kai duotas ne array: boolean', () => {
+    expect(average(false)).toBe(false);
+})
+
+test('vidurkis, kai duotas ne array: number', () => {
+    expect(average(8)).toBe(false);
+})
+
+test('vidurkis, kai duotas ne array: null', () => {
+    expect(average(null)).toBe(false);
+})
+
+test('vidurkis, kai duotas ne array: object', () => {
+    expect(average({})).toBe(false);
+})
+
+test('vidurkis, kai duotas tuscias array', () => {
+    expect(average([])).toBe(false);
+})
+
+test('vidurkis, kai ingoruojami array esantys: string', () => {
+    expect(average([8, 'a', 6])).toBe(7);
+})
+
+test('vidurkis, kai ingoruojami array esantys: boolean', () => {
+    expect(average([8, true, 6])).toBe(7);
+})
+
+test('vidurkis, kai ingoruojami array esantys: boolean', () => {
+    expect(average([8, false, 6])).toBe(7);
+})
+
+test('vidurkis, kai ingoruojami array esantys: NaN', () => {
+    expect(average([8, NaN, 6])).toBe(7);
+})
+
+test('vidurkis, kai ingoruojami array esantys: []', () => {
+    expect(average([8, [], 6])).toBe(7);
+})
+
+test('vidurkis, kai ingoruojami array esantys: {}', () => {
+    expect(average([8, {}, 6])).toBe(7);
+})
+
+test('vidurkis, kai ingoruojami array esantys: null', () => {
+    expect(average([8, null, 6])).toBe(7);
+})
+
+test('vidurkis, kai ingoruojami array esantys: undefined', () => {
+    expect(average([8, undefined, 6])).toBe(7);
+})
+
+test('vidurkis, kai ingoruojami array esantys: function', () => {
+    expect(average([8, average, 6])).toBe(7);
+})
